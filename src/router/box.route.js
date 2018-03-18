905e6ce7cf7810a062ea0b6106b2dc7bf1001fb0
Antsiferov Maxim <antsiferovmaximv@gmail.com>
2018-03-18 22:37:19 +0200

    Added box pass

--- src/router/box.route.js
+++ src/router/box.route.js
@@ -6,7 +6,9 @@ module.exports = function () {
     return router
         .get('/box/:id', async function (req, res) {
             const box = await Box.findById(req.params.id);
-            
+
+            box.password = '';
+
             res.send({msg: '', box});
         })
         .get('/box/all/:page', async function (req, res) {
@@ -27,9 +29,60 @@ module.exports = function () {
 
             res.send('sdas');
         })
+        .post('/box/join', async function (req, res) {
+            const {userId, boxId, password} = req.body;
+
+            const box = await Box.findById(boxId);
+
+            if (!box) {
+                res.send({msg: 'Коробка не найдена', isJoin: false});
+                return;
+            }
+
+            if (box.isPrivate && box.password !== password) {
+                res.send({msg: 'Не верный пароль', isJoin: false});
+                return;
+            }
+
+            const users = box.users;
+            let msg;
+
+            users.push(userId);
+            msg = 'Вы вступили в коробку';
+
+            await Box.findByIdAndUpdate(boxId, {$set: {users}}, {new: false});
+
+            res.send({msg, isJoin: true});
+        })
+        .post('/box/leave', async function (req, res) {
+            const {userId, boxId, password} = req.body;
+
+            const box = await Box.findById(boxId);
+
+            if (!box) {
+                res.send({msg: 'Коробка не найдена', isJoin: false});
+                return;
+            }
+
+            if (box.isPrivate && box.password !== password) {
+                res.send({msg: 'Не верный пароль', isJoin: false});
+                return;
+            }
+
+            const users = box.users;
+            const userIndex = users.indexOf(userId);
+            let msg;
+
+            users.splice(userIndex, 1);
+            msg = 'Вы вышли из коробки';
+
+            await Box.findByIdAndUpdate(boxId, {$set: {users}}, {new: false});
+
+            res.send({msg, isJoin: true});
+        })
         .post('/box/favorite', async function (req, res) {
             const {userId, boxId} = req.body;
-            
+
             const user = await User.findById(userId);
             const favoritesBox = user.favoritesBox;
             const index = favoritesBox.indexOf(boxId);
@@ -39,8 +92,8 @@ module.exports = function () {
             } else {
                 favoritesBox.splice(index, 1)
             }
-            
-            await User.findByIdAndUpdate(userId, {$set: {favoritesBox}}, { new: false });
+
+            await User.findByIdAndUpdate(userId, {$set: {favoritesBox}}, {new: false});
 
             res.send({msg: 'Фаворит выбран', favoritesBox});
         })
