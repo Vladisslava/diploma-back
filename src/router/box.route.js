f6a0e2edcb2180a2f3599f5fe30339fa04705eb3
Antsiferov Maxim <antsiferovmaximv@gmail.com>
2018-03-26 13:31:05 +0300

    Added user boxes

--- src/router/box.route.js
+++ src/router/box.route.js
@@ -4,6 +4,17 @@ const User = require('../database/models/user.model');
 
 module.exports = function () {
     return router
+        .get('/box/join', async function (req, res) {
+            const {userId, boxId} = req.query;
+
+            console.log(userId, boxId);
+
+            const box = await Box.findById(boxId);
+
+            const isJoined = box.users.includes(userId);
+
+            res.send({msg: '', isJoined})
+        })
         .get('/box/:id', async function (req, res) {
             const box = await Box.findById(req.params.id);
 
@@ -13,8 +24,21 @@ module.exports = function () {
         })
         .get('/box/all/:page', async function (req, res) {
             const boxes = await Box.paginate({}, {page: req.params.page, limit: 6});
-
+            
             res.send({msg: '', boxes})
+        })
+        .get('/box/user/:id', async function (req, res) {
+            const boxes = await Box.paginate({users: req.params.id}, {page: req.query.page, limit: 6});
+
+            res.send({msg: 'User boxes', boxes})
+        })
+        .get('/box/favorite/:id', async function (req, res) {
+            const user = await User.findById(req.params.id);
+            const boxesId = user.favoritesBox;
+
+            const boxes = await Box.paginate({_id: {$in: boxesId}}, {page: req.query.page, limit: 6});
+
+            res.send({msg: 'favorite', boxes: boxes})
         })
         .post('/box', async function (req, res) {
             try {
@@ -47,7 +71,10 @@ module.exports = function () {
             const users = box.users;
             let msg;
 
-            users.push(userId);
+            if (users.indexOf(userId) === -1) {
+                users.push(userId);
+            }
+
             msg = 'Вы вступили в коробку';
 
             await Box.findByIdAndUpdate(boxId, {$set: {users}}, {new: false});
@@ -64,10 +91,10 @@ module.exports = function () {
                 return;
             }
 
-            if (box.isPrivate && box.password !== password) {
+            /*if (box.isPrivate && box.password !== password) {
                 res.send({msg: 'Не верный пароль', isJoin: false});
                 return;
-            }
+            }*/
 
             const users = box.users;
             const userIndex = users.indexOf(userId);
